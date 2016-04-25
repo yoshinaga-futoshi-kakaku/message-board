a7ecce6d0ef483759cec88e22b3e6233b4240b36
futoshi <yoshinaga.futoshi.kakaku@gmail.com>
2016-04-25 04:45:51 +0000

    kadai6-9

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422071919) do
+ActiveRecord::Schema.define(version: 20160425042115) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
