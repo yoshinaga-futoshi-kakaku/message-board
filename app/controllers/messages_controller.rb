c8654762e2a9c00073bf7e2e029d34fe9c8ddf49
futoshi <yoshinaga.futoshi.kakaku@gmail.com>
2016-04-25 02:43:54 +0000

    edit,delete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     @message = Message.new
     @messages= Message.all
@@ -15,8 +16,28 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: "message wo hensyu sitayo"
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: "削除したよ"
+  end
+  
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
